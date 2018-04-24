bf46702ce771f8ae9dca78a3838c7ff0dcbc640e
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2018-04-24 14:27:33 +0200

    Checks if exclusion string appears in database before creating campaign

--- includes/methods/feed/feed.processor.php
+++ includes/methods/feed/feed.processor.php
@@ -5,7 +5,7 @@ $productions = array();
 
 // Sanitize & trim string
 function trimString($string) {
-	return filter_var(trim($string), FILTER_SANITIZE_STRING);
+	return str_replace(array("&#39;", "/"),array("'","-"), filter_var(trim($string), FILTER_SANITIZE_STRING));
 }
 
 //Sanitize genre array
@@ -28,27 +28,45 @@ foreach ($xml->production as $production) {
 	$time = strtotime(filter_var($lastShow->start, FILTER_SANITIZE_STRING));
 	// Filter by month
 	if (date('Y-m', $time) == $month) {
-		//Check if genre exist
-		if ($production->genres->genre) {
 			$productionObj = new stdClass();
+			
+			//Check if venue of production matches value stored in db
+			if (stripos($location['venue'][0], trimString($production->location->venue)) === false) {
+				$location['venue'] = array();
+				$location['venue'] = trimString($production->location->venue);
+			} 
+			
 			// Put data to object
 			$productionObj->title = trimString($production->title);
 			$productionObj->subtitle = trimString($production->subtitle);
-			$productionObj->venue = trimString($production->location->venue);
-			$productionObj->location = $location;
+			$productionObj->venue = $location['venue'];
+			$productionObj->location = $location['city'];
 			$productionObj->genre = listGenres($production->genres->genre);
+			//Check if genre exist
+			if (count($production->genres->genre) < 1) {
+				$productionObj->genre[0] = 'overig';
+			}
 			$productionObj->link = filter_var(trim($production->link), FILTER_SANITIZE_URL);
 			$productionObj->date->time = $time;
 			$productionObj->date->dateString = date('d-m-Y H:i', $productionObj->date->time);
-			$productionObj->performers = getPerformers($productionObj->link);
+			$productionObj->performers = getPerformers($productionObj->link, null);
 
 			// Push object to productions array
 			// Exclude productions with irrelevant tags, title or sold out
-			if (stripos($exclude, $productionObj->genre[0]) === false && stripos($exclude, $productionObj->genre[1]) === false && stripos($exclude, $productionObj->title) === false && stripos($productionObj->title, 'inleiding') === false && $lastShow->status !== "Uitverkocht" && $lastShow->status !== "Geannuleerd") {
-				array_push($productions, $productionObj);
+			if (stripos($exclude, $productionObj->genre[0]) === false && stripos($exclude, $productionObj->genre[1]) === false && stripos($exclude, $productionObj->title) === false && stripos($productionObj->title, 'inleiding') === false && stripos($lastShow->status, "uitverkocht") === false && $lastShow->status !== "Geannuleerd") {
+				$excludeFound = false;
+				
+				foreach (explode(' ', $exclude) as $excl) {
+					if (stripos($productionObj->title, $excl) > -1) {
+						$excludeFound = true;
+					}
+				}
+				
+				if ($excludeFound == false) {
+					array_push($productions, $productionObj);
+				}
 			}
 		}
 
-	}
 }
 ?>
